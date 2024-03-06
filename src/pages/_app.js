055aeb90d2fae18b6633734d3d3a51bf0a3edf13
maximoortelli <maximoortelli@hotmail.com>
2024-03-06 14:08:23 -0300

    Updated _app.js
    Updated _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import {store} from "../app/store";
+import { Provider } from "react-redux";
 
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+         <Component {...pageProps} />
+      </Provider>
     </SessionProvider>
   );
 };
